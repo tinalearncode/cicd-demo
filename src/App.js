52e9226d65c8df5f1742262630eee9e8412c45f7
Tina Gao <tina.hm.gao@gmail.com>
2022-03-03 23:26:55 +1300

    delete file to trim the container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Tina is learning👀 React⚛ CI/CD⛓ Docker🐬 and more 🌱
         </p>
